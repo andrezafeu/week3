cee61643a89a9ce900cbade712df427d99a8937d
Andreza Feu <andrezafeu@gmail.com>
2016-04-15 16:03:36 -0400

    Changed while loop for a recursive one and fixed errors

--- quiz_game/quiz.js
+++ quiz_game/quiz.js
@@ -1,51 +1,37 @@
 var read = require('read');
-
-question1 = {
-    prompt: "What's your name?\n>",
-    correctAnswer: "Andreza"
-}
-
-question2 = {
-	prompt: "Is Miami a big city?",
-	correctAnswer: "Yes"
-}
-
-question3 = {
-	prompt: "What is JS?",
-	correctAnswer: "JavaScript"
-}
-
-question4 = {
-	prompt: "Which city is Ironhack at?",
-	correctAnswer: "Miami"
-}
-
-question5 = {
-	prompt: "To which language belongs a .rb file?",
-	correctAnswer: "Ruby"
-}
-
-function displayName (err, input){
-	if (err) {
-        throw err;
-    } else {
-    checkAnswer(input)
-	}
-}
+var Question = require('./question.js');
 
 var count = 1;
 
-function checkAnswer(input) {
+
+question1 = new Question("What's your name?\n>", "Andreza", 1);
+question2 = new Question("Is Miami a big city?", "Yes", 2);
+question3 = new Question("What is JS?", "JavaScript", 3);
+question4 = new Question("Which city is Ironhack at?", "Miami", 4);
+question5 = new Question("To which language belongs a .rb file?", "Ruby", 5);
+
+currentQuestion = eval("question"+count)
+function displayAnswer (err, input){
+	if (err) {
+        throw err;
+    } else {
+    		currentQuestion.checkAnswer(input);
+    		count = count + 1;
+    	if (count <=5) {
+			currentQuestion = eval("question"+count)
+			read(currentQuestion,displayAnswer)
+    	} else {
+			console.log("You've finished the Quiz! Congratulations!")
+    	}
+    }
+}	
+
+Question.prototype.checkAnswer = function(input) {
 	if (currentQuestion.correctAnswer !== input) {
 		console.log("Incorrect Answer");
 	} else {
-		console.log("Your  name is: " + input)
+		console.log("Your answer is: " + input)
 	}
 }
 
-while (count <= 5) {
-	currentQuestion = eval("question"+count)
-	read(currentQuestion,displayName)
-	count = count + 1;
-} 
-	console.log("You've finished the Quiz! Congratulations!")
+read(currentQuestion,displayAnswer)

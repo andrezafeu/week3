b8a1102355d33fe84e3afb1d6a6a88c506378abf
Andreza Feu <andrezafeu@gmail.com>
2016-04-14 23:35:09 -0400

    Inluded the number os star according to the rating rounded down.

--- js_vs_ruby_episodes-master/episode.js
+++ js_vs_ruby_episodes-master/episode.js
@@ -18,21 +18,10 @@ function loopAndSort(episodes){
 	var realEpisodes = [];
 
 	episodes.forEach(function(old_ep){
-		var newEp = new Episode(old_ep.title, old_ep.episode_number, old_ep.description, old_ep.rating)
+		var newEp = new Episode(old_ep.title, old_ep.episode_number, old_ep.description, old_ep.rating);
 		realEpisodes.push(newEp);
+
 	})
-
-	// var stars = " ";
-	// for (j = 0; j <= 9; j++) {
-	// var ratings = Math.floor(episodes[j].rating);
-	// 	// episodes[j];
-	// 	// for (i = 1; i <= ratings; i++) {
-	// 	// 	stars = stars + "*";
-	// 	// 	console.log(stars);
-	// 	// }
-	// }
-
-
 
 	return realEpisodes;
 }
@@ -48,6 +37,12 @@ Episode.prototype.print = function(){
 	console.log(`${this.episode_number}: ${this.title}` )
 	console.log(`${this.description}` )
 	console.log(`${this.rating}` )
+	stars = "";
+	for(i=0; i<Math.floor(`${this.rating}`);i++) {
+		stars = stars + "*"
+	}
+	console.log(stars);
+
 }
 
 var EpisodeList = function(episodes){
